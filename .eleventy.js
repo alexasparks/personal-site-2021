7655878cfe3b7a26908dc2ab92a74153e9219a71
Alexa <alexa.sparks@wpengine.com>
2021-01-09 06:37:28 -0600

    initial setup

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+const Nunjucks = require('nunjucks');
+
+module.exports = function(eleventyConfig) {
+    const nunjucksEnvironment = new Nunjucks.Environment( new Nunjucks.FileSystemLoader('_includes'));
+    eleventyConfig.setLibrary('njk', nunjucksEnvironment);
+
+    eleventyConfig.addPassthroughCopy('styles');
+}
---
+++ index.md
@@ -0,0 +1,6 @@
+---
+layout: home.njk
+title: Alexa Sparks
+---
+
+## Software developer
